986d63a305c90acf87b1fed90c118760331091c2
rohit nagappa <rnagappa35@gmail.com>
2020-09-23 22:53:13 +0530

    absence of failure is success in junit test

---
+++ test/com/nagappa/MyMathTest.java
@@ -0,0 +1,15 @@
+package com.nagappa;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyMathTest {
+
+	@Test
+	public void test() {
+	//	fail("Not yet implemented");
+		//Absence of failure is success
+	}
+
+}
