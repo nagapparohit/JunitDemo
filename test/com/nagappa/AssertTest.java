31cfbb2793af4ff65071325914f36a899c9b3e26
rohit nagappa <rnagappa35@gmail.com>
2020-09-23 23:11:26 +0530

    differ assert methodv vaia

---
+++ test/com/nagappa/AssertTest.java
@@ -0,0 +1,23 @@
+package com.nagappa;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AssertTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		//assert true can be used to check whether a condition is true or not
+		//assertTrue(condition);
+		//we have asset assertFalse()
+		
+		//here we are doing static import of asset methods so can directly use
+		//user to check where values null or not
+		assertNotNull(null);
+		//it used to compare whether two arrays are equal or not
+		//assertArrayEquals(null, null);
+	}
+
+}
