cfc228acb44e012283fd57b74f4e409e0d6d4bb4
rohit nagappa <rnagappa35@gmail.com>
2020-09-23 23:20:01 +0530

    added before which is run before any test as junit doesnot gurantee order in which test is run

--- test/com/nagappa/MyMathTest.java
+++ test/com/nagappa/MyMathTest.java
@@ -2,18 +2,31 @@ package com.nagappa;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MyMathTest {
-	
+	/**
+	 * There is no gurantee in unit test which test run before which test
+	 * 
+	 * All that junit gurantee is that test will be , order is not that guarateed
+	 */
 	//instead of creating mymath instance in every junit test we can create it here
 	MyMath myMath = new MyMath();
+	/**
+	 * @before mean this will be running before every test
+	 */
+	@Before
+	public void before() {
+		System.out.println("Before");
+	}
 	//MyMath.sum
 	@Test
 	public void sum_with3numbers() {
 	//	fail("Not yet implemented");
 		//Absence of failure is success
 		//MyMath myMath = new MyMath();
+		System.out.println("test1");
 		int result = myMath.sum(new int[] {1,2,3});
 		
 		assertEquals(6,result);
@@ -22,7 +35,8 @@ public class MyMathTest {
 	
 	@Test
 	public void sum_with1number() {
-	//	MyMath myMath = new MyMath();
+		System.out.println("test2");
+		//	MyMath myMath = new MyMath();
 		int result = myMath.sum(new int[] {3});
 		
 		assertEquals(3,result);
