6d00c82affc75270bab1efa0f81e4326b9b70387
rohit nagappa <rnagappa35@gmail.com>
2020-09-23 23:02:12 +0530

    simpler unit test

--- test/com/nagappa/MyMathTest.java
+++ test/com/nagappa/MyMathTest.java
@@ -5,11 +5,29 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MyMathTest {
-
+	
+	//instead of creating mymath instance in every junit test we can create it here
+	MyMath myMath = new MyMath();
+	//MyMath.sum
 	@Test
-	public void test() {
+	public void sum_with3numbers() {
 	//	fail("Not yet implemented");
 		//Absence of failure is success
+		//MyMath myMath = new MyMath();
+		int result = myMath.sum(new int[] {1,2,3});
+		
+		assertEquals(6,result);
+		
 	}
+	
+	@Test
+	public void sum_with1number() {
+	//	MyMath myMath = new MyMath();
+		int result = myMath.sum(new int[] {3});
+		
+		assertEquals(3,result);
+		
+	}
+
 
 }
